31d42ee7961ae44389cac5bd628f95a42ca67bc4
yuminc03 <yuminc03@gamil.com>
2021-07-26 22:35:13 +0900

    2021-07-26: router를 구현하여 사용자가 어떤 주소로 들어올때 주소에 해당하는 페이지 보여주기

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { HashRouter, Route, Switch, NavLink } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Home(){
+  return(
+    <div>
+      <h2>Home</h2>
+      Home...
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Topies(){
+  return(
+    <div>
+      <h2>Topics</h2>
+      Topies...
+    </div>
+  )
+}
+
+function Contact(){
+  return(
+    <div>
+      <h2>contact</h2>
+      Contact...
+    </div>
+  )
+}
+
+function App(){
+  return(
+    <div>
+      <h1>React Router DOM</h1>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/topics">Topies</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+      </ul>
+      <ul>
+        <Switch>
+          <Route exact path="/"><Home></Home></Route> 
+          <Route path="/topics"><Topies></Topies></Route>
+          <Route path="/contact"><Contact></Contact></Route>
+        </Switch>
+      </ul>
+      {/* <ul>
+        <Home></Home>
+        <Topies></Topies>
+        <Contact></Contact>
+      </ul> */}
+    </div>
+  )
+}
+
+ReactDOM.render(<HashRouter><App/></HashRouter>, document.getElementById('root'));
+
